574729cb3867c43bec8eb4f5bb65a6054637c11e
pratyakshchauhan1 <97598542+pratyakshchauhan1@users.noreply.github.com>
2023-08-07 18:33:18 +0530

    Add files via upload

---
+++ wowow.py
@@ -0,0 +1,75 @@
+import cv2
+
+cv2.imread("solar-system.jpg")
+cv2.waitKey(0)
+cv2.putText(img,
+           "Sun",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Mercury",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Venus",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Earth",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Mars",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Jupiter",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Saturn",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Uranus",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
+
+cv2.putText(img,
+           "Neptune",
+            (220, 300), 
+            cv2.FONT_HERSHEY_COMPLEX,
+            0.5,
+            (255,255,255)
+            )
